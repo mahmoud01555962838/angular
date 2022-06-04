476b8f92d494476d5f4d18ddde868194c6dee46a
محمود عبدالباسط <mahmoud01555962838@gmail.com>
2022-06-04 23:16:41 +0200

    angular lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SectionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
